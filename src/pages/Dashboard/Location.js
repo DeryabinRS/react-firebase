be395f718f9231963235ca4d57c53d9425667fa1
DeryabinRS <deryabinrs@gmail.com>
2021-09-09 17:38:35 +0700

    Location component added

--- src/pages/Dashboard/Location.js
+++ src/pages/Dashboard/Location.js
@@ -1,35 +1,67 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { db } from '../../utils/firebase' 
 import { 
   Button, 
   Tr,
-  Td
+  Td,
+  Input
 } from "@chakra-ui/react";
 
 const Location = ({location}) => {
-	const {id, name, city, region} = location
+	// Initializing didMount as false
+	const [didMount, setDidMount] = useState(false)
+
+	const [newLocation, setNewLocate] = useState({
+		name: location.name,
+		city: location.city,
+		region: location.region
+	});
+
+	// Setting didMount to true upon mounting
+	useEffect(() => setDidMount(true), [])
+
+	useEffect(() => {
+		if (didMount) handleChangeInputValueInDB()
+	}, [newLocation]);
+
 	const deleteLocation = async() => {
 		try{
-		await db.collection('locations').doc(id).delete();
+		await db.collection('locations').doc(location.id).delete();
 		}catch(err){
 		console.log(err)
 		}
 	}
 
-	const handleChangeInput = (e) => {
-		
+	const onChangeHandler = ({ target: { value, name } }) => {
+		setNewLocate({
+			...newLocation,
+			[name]: value
+		})
+	}
+
+	async function handleChangeInputValueInDB(){
+		try {
+			await db.collection('locations').doc(location.id)
+				.update({
+					name: newLocation.name,
+					city: newLocation.city,
+					region: newLocation.region
+				})
+		} catch (error) {
+			console.error(error);
+		}
 	}
 
 	return (
 		<Tr>
 			<Td>
-			{name}
+				<Input value={newLocation.name} name="name" onChange={onChangeHandler}/>
 			</Td>
 			<Td>
-			{city}
+				<Input value={newLocation.city} name="city" onChange={onChangeHandler}/>
 			</Td>
 			<Td>
-			{region}
+				<Input value={newLocation.region} name="region" onChange={onChangeHandler}/>
 			</Td>
 			<Td>
 			<Button
